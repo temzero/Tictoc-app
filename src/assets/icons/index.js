ed9d424f3de3729d661ba31e07fe4c309bfd8962
temzero <nguyentrannhan.00@gmail.com>
2024-09-25 21:31:40 +0700

    64. Header UI (1)

---
+++ src/assets/icons/index.js
@@ -0,0 +1,7 @@
+const icons = {
+   logo: require('./tictoc.svg').default,
+   close: require('./close-circle-svgrepo-com.svg').default,
+   search: require('./search-magnifying-glass-svgrepo-com.svg').default,
+};
+
+export default icons;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,27 @@
+import icons from '../../../../assets/icons';
 import styles from './Header.module.scss';
 
 function Header() {
-    return ( <header className={styles.wrapper}>
-        <div className={styles.inner}>
-            
-        </div>
-    </header> );
+   return (
+      <header className={styles.wrapper}>
+         <div className={styles.inner}>
+            <img className={styles.logoImage} src={icons.logo} alt="Tictoc" />
+            <div className={styles.search}>
+                <input type='text' placeholder='Search for accounts and videos' />
+                <button className={styles.clearButton}>
+                  <img className={styles.clearIcon} src={icons.close} alt="Close" />
+
+                </button>
+                <button className={styles.searchButton}>
+                   <img className={styles.searchIcon} src={icons.search} alt="Close" />
+                </button>
+            </div>
+            <div className={styles.actions}>
+
+            </div>
+         </div>
+      </header>
+   );
 }
 
 export default Header;
